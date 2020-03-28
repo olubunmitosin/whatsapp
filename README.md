c2bde39622135c9512a6c9fc73f8832b6c9ed1e4
Olubunmi Tosin <olubunmivictor6@gmail.com>
2020-03-28 09:33:45 +0100

    Initial and reviewd commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Whatsapp
+
+An unofficial whatsapp application for Linux Destros
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://gitlab.com/olubunmi708/whatsapp
+# Go into the repository
+cd whatsapp
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+
+## License
+
+[CC0 1.0 (Public Domain)](LICENSE.md)
---
+++ main.js
@@ -0,0 +1,140 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, Menu, Tray, ipcMain, screen, session} = require('electron')
+const path = require('path')
+
+
+
+let mainWindow;
+let tray = Tray;
+let menu = Menu;
+let appName = 'WhatsApp';
+let appIcon = path.join(__dirname, 'images', 'app.png');
+let appIconEvent = path.join(__dirname, 'images', 'app_event.png');
+let ipc = ipcMain;
+let sysTray;
+let isQuitting = false;
+let unreadNotification = false;
+let win;
+
+// Change these to customize the app
+let url = 'https://web.whatsapp.com/';
+let height = 750;
+let width = 1200;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: width,
+    height: height,
+    icon: appIcon,
+    webPreferences: {
+      nodeIntegration: false, // fails without this because of CommonJS script detection
+      preload: path.join(__dirname, 'js', 'browser.js')
+    }
+  })
+
+
+  //remove default menu tray
+  win.setMenu(null);
+
+  //Set a default user agent
+  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+    details.requestHeaders['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36';
+    callback({ cancel: false, requestHeaders: details.requestHeaders });
+  });
+
+  // and load the url of the app.
+  win.loadURL(url)
+
+  win.on('focus', e => {
+    if (unreadNotification) {
+      unreadNotification = false;
+      sysTray.setImage(appIcon);
+    }
+  });
+
+  win.on('close', e => {
+    if (!isQuitting) {
+      e.preventDefault();
+      win.hide();
+    }
+  });
+
+
+  return win;
+}
+
+
+function showAndCenter(win) {
+  center(win);
+  win.show();
+  win.focus();
+}
+
+function center(win) {
+  let electronScreen = screen;
+  let size = electronScreen.getPrimaryDisplay().workAreaSize;
+  let x = Math.round(size.width / 2 - width / 2);
+  let y = Math.round(size.height / 2 - height / 2);
+  win.setPosition(x, y);
+}
+
+setProcess = function() {
+  sysTray = new tray(appIcon);
+  let contextMenu = menu.buildFromTemplate([
+    { label: 'Show', click: function() { showAndCenter(mainWindow); } },
+    { label: 'Quit', click: function() { app.quit(); } }
+  ]);
+  sysTray.setToolTip(appName);
+  sysTray.setContextMenu(contextMenu);
+
+  sysTray.on('click', () => {
+    showAndCenter(mainWindow);
+  });
+
+  mainWindow = createWindow();
+
+  let page = mainWindow.webContents;
+
+  page.on('dom-ready', () => {
+    showAndCenter(mainWindow);
+  });
+
+  page.on('new-window', (e, url) => {
+    e.preventDefault();
+    app.shell.openExternal(url);
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(setProcess)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  showAndCenter(mainWindow);
+});
+
+app.on('before-quit', () => {
+  isQuitting = true;
+});
+
+ipc.on('change-icon', () => {
+  if (!unreadNotification) {
+    unreadNotification = true;
+    sysTray.setImage(appIconEvent);
+  }
+});
+
+ipc.on('notification-click', () => {
+  showAndCenter(mainWindow);
+});
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
