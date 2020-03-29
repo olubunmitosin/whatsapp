38b957cd16c9036ec927cce794afbda9cd6535a2
Olubunmi Tosin <olubunmivictor6@gmail.com>
2020-03-29 22:31:48 +0100

    resolved snapcraft build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME=whatsapp
 VERSION=1.2.0
-electron-packager . $APPNAME --platform=linux --arch=x64 --app-version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
+electron-packager . $APPNAME --platform=linux --arch=x64 --app-version=$VERSION --overwrite=true --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
