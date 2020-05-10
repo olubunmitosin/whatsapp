3b57edb45c7e7e1179cace43e9d83cbc86d9c282
Olubunmi Tosin <olubunmivictor6@gmail.com>
2020-05-10 15:21:39 +0100

    included support for x64, arm64 and armhf linux targets

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-APPNAME=whatsapp
-VERSION=1.2.3
-electron-packager . $APPNAME --platform=linux --arch=x64 --app-version=$VERSION --overwrite=true --app_version=1.2.3 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
